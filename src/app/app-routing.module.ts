931632dcb583f05d1813cad483b3c50dfdf5add2
Batroz12 <davicoasc@gmail.com>
2022-10-28 12:06:17 -0500

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { AgregarProductoAlmacenComponent } from './agregar-producto-almacen/agregar-producto-almacen.component';
 import { VerEmpleadosComponent } from './ver-empleados/ver-empleados.component';
 
 const routes: Routes = [
 
   {path:'Agregar-Empleado', component: AgregarEmpleadoComponent},
   {path:'Ver-Empleado', component: VerEmpleadosComponent},
+  {path:'Almacen', component: AgregarProductoAlmacenComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { AppComponent } from './app.component';
 import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
 import { VerEmpleadosComponent } from './ver-empleados/ver-empleados.component';
 import { CaracteristicasEmpleadoComponent } from './caracteristicas-empleado/caracteristicas-empleado.component';
+import { AgregarProductoAlmacenComponent } from './agregar-producto-almacen/agregar-producto-almacen.component';
+import { VerAlmacenComponent } from './ver-almacen/ver-almacen.component';
+import { CaracteristicaAlmacenComponent } from './caracteristica-almacen/caracteristica-almacen.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AgregarEmpleadoComponent,
     VerEmpleadosComponent,
-    CaracteristicasEmpleadoComponent
+    CaracteristicasEmpleadoComponent,
+    AgregarProductoAlmacenComponent,
+    VerAlmacenComponent,
+    CaracteristicaAlmacenComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
